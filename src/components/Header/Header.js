449e2b421a9705dcb9fd34245970636974b59abe
Tifenn <tifenn.queudet@gmail.com>
2021-06-04 20:40:06 +0200

    Init header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    const [navLinks, setNavLinks] = useState([
+        {id: 1, title: 'A propos'},
+        {id: 2, title: 'Services'},
+        {id: 3, title: 'Situations'},
+        {id: 4, title: 'Tarifs'},
+        {id: 5, title: 'Actualités'},
+        {id: 6, title: 'Prendre rendez-vous'},
+    ])
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
